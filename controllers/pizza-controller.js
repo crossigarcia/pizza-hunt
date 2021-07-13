ee6bb4affdfa45966d98617ec3d4a636d2de8b90
crosgar <cecilia.rossi.garcia@gmail.com>
2021-07-13 16:14:09 -0700

    resolved bug in controllers file, connected front end to back end, create pizza form now works

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -1,4 +1,4 @@
-const Pizza = require('../models');
+const { Pizza } = require('../models');
 
 const pizzaController = {
    //the functions will go in her as methods
--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const express = require("express");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.use(require('./routes'));
+app.use(require("./routes"));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pizza-hunt', {
-   useFindAndModify: false,
-   useNewUrlParser: true,
-   useUnifiedTopology: true
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/pizza-hunt", {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 //use this to log mongo queries being executed!
